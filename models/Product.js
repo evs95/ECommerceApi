dc3e5348141a2efd455aa3241a92a3be1ae471c2
evs95 <elamarthy95@gmail.com>
2022-04-02 14:12:07 -0400

    Code changes for routes, ORM

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,38 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price:{
+      type:DataTypes.DECIMAL,
+      allowNull:false,
+      validate:{
+        isDecimal:true
+      }
+    },
+    stack:{
+      type:DataTypes.INTEGER,
+      defaultValue:10,
+      validate:{
+        isNumeric:true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // This references the `category` model, which we set in `Category.js` as its `modelName` property
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
